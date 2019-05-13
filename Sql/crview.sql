fead557ea586bfd9bbca104b10ffec95ddd19c81
Frans Mazhar <frans.mazhar@mii.co.id>
2019-05-13 15:01:40 +0700

    d

--- Sql/crview.sql
+++ Sql/crview.sql
@@ -25,7 +25,7 @@ sales_hierarchy as
 (  SELECT username, CONNECT_BY_ROOT username AS AuthAppr, LEVEL-1 AS Pathlen
    FROM sec_user
    WHERE department = 2
-   CONNECT BY PRIOR id = report_to
+   CONNECT BY PRIOR id = report_to_id
 )
 SELECT
 r.dcv_hdr_id
@@ -84,5 +84,70 @@ LEFT OUTER JOIN task_query t
 ON
 t.no_dcv = r.no_dcv;
 
-CREATE OR REPLACE FORCE VIEW "DCV"."UOM_LIST" ("UOMCODE", "UOMDESC", "UOMTYPE") AS
-select 'uom_code' uomcode, 'uom_desc' uomdesc, 'uom_type' uomtype from dual;
+
+CREATE VIEW pc_produk_vw AS
+SELECT p.proposal_id
+, p.proposal_no
+, p.proposal_date
+, p.periode_prog_from
+, p.periode_prog_to
+, p.prog_promo
+, p.proposal_type
+, p.discount_type
+, p.mekanisme_penagihan
+, p.status
+, p.pemohon
+, p.confirm_no
+, p.user_type_creator
+, p.report_run_number
+, pp.product_category
+, pp.product_category_desc
+, pp.product_class
+, pp.product_class_desc
+, pp.product_brand
+, pp.product_brand_desc
+, pp.product_ext
+, pp.product_ext_desc
+, pp.product_pack
+, pp.product_pack_desc
+FROM promo_produk pp
+, proposal p
+WHERE p.proposal_id = pp.proposal_id
+AND NVL(p.confirm_no,'Auto Generated') <> 'Auto Generated';
+
+
+CREATE OR REPLACE FORCE EDITIONABLE VIEW MASTER_CUSTOMER AS
+select
+  a.customer_id as cust_id,
+  a.customer_number as cust_code,
+  a.customer_name as cust_name,
+  (a.customer_number || ' - ' || a.customer_name || ' - ' || b.description) as cust_fullname,
+  a.attribute3 as region_code,
+c.description as region_name,
+  (a.attribute3 || ' ' || c.description) as region_fullname,
+  a.attribute4 as area_code,
+d.description as area_name,
+  (a.attribute4 || ' ' || d.description) as area_fullname,
+  a.attribute5 as location_code,
+b.description as location_name,
+  (a.attribute5 || ' ' || b.description) as location_fullname,
+  a.attribute8 as type_code,
+f.description as type_name,
+  (a.attribute8 || ' ' || f.description) as type_fullname,
+  a.attribute1 as group_code,
+e.description as group_name,
+  (a.attribute1 || ' ' || e.description) as group_fullname,
+a.status as cust_status
+from
+  apps.ar_customers a,
+  apps.fcs_flex_values_vl b,
+  apps.fcs_flex_values_vl c,
+  apps.fcs_flex_values_vl d,
+  apps.fcs_flex_values_vl e,
+  apps.fcs_flex_values_vl f
+where a.attribute5 = b.flex_value
+  and a.attribute3 = c.flex_value
+  and a.attribute4 = d.flex_value
+  and a.attribute1 = e.flex_value
+  and a.attribute8 = f.flex_value
+order by region_code, area_code, location_code, cust_fullname;
